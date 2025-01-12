048c7e28bcbca2d1ddd0d76760ec73e07d593c50
Aleksander Jenssen <113600268+Aleks901@users.noreply.github.com>
2025-01-12 13:43:50 +0100

    Dekryptering
    La til dekrypterings metode for den tidligere nevnte krypterings metoden.

--- Oblig_01/src/Oblig_01.java
+++ Oblig_01/src/Oblig_01.java
@@ -4,6 +4,7 @@ public class Oblig_01 {
 
     public static void main(String[] args) {
         System.out.println(Krypter("Karmann-Ghia"));
+        System.out.println(Dekrypter("nXvnueTz-naa"));
     }
 
     public static String ROT13(String S)
@@ -22,25 +23,62 @@ public class Oblig_01 {
     }
 
     public static String Krypter(String S) {
+        // Steg 1 av krypteringen
         S = ROT13(S);
 
         Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
         String T = "";
 
+        // Lagrer bokstav av strengen i et character array
         char[] characters_in_S = S.toCharArray();
+        // Steg 2 av krypteringen
         for (int i = 0; i < S.length() / 2; i++) {
             queue.add(characters_in_S[i]);
         }
+        // Steg 3 av krypteringen
         for (int i = S.length() / 2; i < S.length(); i++) {
             stack.push(characters_in_S[i]);
         }
-
+        // Steg 4 av krypteringen
         while (!queue.isEmpty() || !stack.isEmpty()) {
             T += stack.pop();
             T += queue.remove();
         }
+        return T;
+    }
 
+    public static String Dekrypter(String S){
+        char[] characters_in_S = S.toCharArray();
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        String T = "";
+
+        // Dekonstruerer strengen med tanke på at hvert oddetall element
+        // er et stack element og hvert partall element er et queue element
+        while(i < characters_in_S.length){
+            stack.push(characters_in_S[i]);
+            i++;
+            queue.add(characters_in_S[i]);
+            i++;
+        }
+
+        // Rekonstruerer strengen med tanke på at halvparten av de
+        // første elementene er fra køen
+        while (!queue.isEmpty()){
+            T += queue.remove();
+        }
+
+        // reverserer stacken for at vi skal få tilbake elementene i
+        // riktig rekkefølge relativt til strengen.
+        // (Kunne helt sikkert vært gjort på en tøffere måte)
+        List<Character> reversed_stack = stack.reversed();
+        for (char c : reversed_stack){
+            T += c;
+        }
+
+        T = ROT13(T);
         return T;
     }
 
