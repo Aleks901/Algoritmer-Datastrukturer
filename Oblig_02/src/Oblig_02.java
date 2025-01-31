e03a2a7ef5f5766fbf300542ad1979ed232f6b66
Aleksander Jenssen <113600268+Aleks901@users.noreply.github.com>
2025-01-31 11:41:05 +0100

    Oblig 2
    La til oblig 2 i emnet.

---
+++ Oblig_02/src/Oblig_02.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Oblig_02
+{
+    public static void lineær(long n)
+    {
+        int tmp = 1;
+        for (long i = 0; i < n; i++)
+            tmp *= 1;
+    }
+
+    public static void kvadratisk(long n)
+    {
+        int tmp = 1;
+        for (long i = 0; i < n; i++)
+            for (long j = 0; j < n; j++)
+                tmp *= 1;
+    }
+
+    public static int logaritmisk(long n)
+    {
+        int tmp = 1, iterasjoner = 0;
+        for (long i = n; i > 0; i /= 2, iterasjoner++)
+            tmp *= 1;
+        return iterasjoner;
+    }
+
+    public static void superlineær(long n)
+    {
+        int tmp = 1;
+        for (long i = 0; i < n; i++)
+            for (long j = n; j > 0; j /= 2)
+                tmp *= 1;
+    }
+
+    public static void kubisk(long n)
+    {
+        int tmp = 1;
+        for (long i = 0; i < n; i++)
+            for (long j = 0; j < n; j++)
+                for (long k = 0; k < n; k++)
+                    tmp *= 1;
+    }
+
+    public static void eksponentiell(long n)
+    {
+        long tmp = 1, iterasjoner = 1;
+
+        for (long i = 0; i < n; i++)
+            iterasjoner *= 2;
+
+        for (long i = 0; i < iterasjoner; i++)
+            tmp *= 2;
+    }
+
+    public static void kombinatorisk(long n)
+    {
+        long tmp = 1, fakultet = 1;
+
+        for (int i = 1; i <= n; i++)
+            fakultet *= i;
+
+        for (long i = 1; i <= fakultet; i++)
+            tmp *= 1;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner S = new Scanner(System.in);
+        long n, T, T1, T2;
+        int valg, iterasjoner = 0;
+
+        System.out.print("1:O(n) 2:O(n²) 3:O(log_n) 4:0(nlog2n) 5:0(n^3) 6:0(Eksponentiell) 7:0(Kombinatorisk)? ");
+        valg = S.nextInt();
+        System.out.print("n? ");
+        n = S.nextLong();
+
+        T1 = System.currentTimeMillis();
+        if (valg == 1)
+            lineær(n);
+        else if (valg == 2)
+            kvadratisk(n);
+        else if (valg == 3)
+            iterasjoner = logaritmisk(n);
+        else if (valg == 4)
+            superlineær(n);
+        else if (valg == 5)
+            kubisk(n);
+        else if (valg == 6)
+            eksponentiell(n);
+        else if (valg == 7)
+            kombinatorisk(n);
+        T2 = System.currentTimeMillis();
+
+        T = T2 - T1;
+        System.out.print("T = " + T+ " ms");
+        if (valg == 3)
+            System.out.print(" (" + iterasjoner + ")");
+        if (valg == 7 && n >= 30)
+            System.out.println(", ...JVM mistet livs lysten");
+        System.out.println();
+    }
+}
