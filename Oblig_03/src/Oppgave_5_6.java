3f44f9d2a98f4413c4e3e0c6679107f713d5aae6
Aleks901 <aleks-j29@hotmail.com>
2025-02-05 17:30:19 +0100

    Tiny justering

--- Oblig_03/src/Oppgave_5_6.java
+++ Oblig_03/src/Oppgave_5_6.java
@@ -10,9 +10,9 @@ public class Oppgave_5_6 {
             pascal[i][0] = 1;
             pascal[i][i] = 1;
 
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j < i; j++)
                 pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
-            }
+
         }
 
         return pascal[n][m];
